36e3f8a0b02b6f791a0c8003f82e4801aef89f57
Devfrank94 <francescomurro94@gmail.com>
2023-04-13 17:49:43 +0200

    completed structure

--- js/script.js
+++ js/script.js
@@ -3,19 +3,25 @@ createApp({
 
 data(){
     return{
-      imagesArray: [
+      images: [
       'img/01.webp',
       'img/02.webp',
       'img/03.webp',
       'img/04.webp',
       'img/05.webp',
-      ]
+      ],
+      counter:0
 
 
     }
-  }
+  },
 
+  methods:{
+    nextPrev(isNext){
+      if(isNext) this.counter++
+    }
 
+  },
 
 
 
