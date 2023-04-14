43e71ff112a8484e9f26a2d23d64ea83d8d5bfe1
Devfrank94 <francescomurro94@gmail.com>
2023-04-15 00:17:11 +0200

    completed all bonus

--- js/script.js
+++ js/script.js
@@ -11,21 +11,36 @@ data(){
       'img/05.webp',
       ],
 
-      counter: 0
-
+      counter: 0,
+      autoScroll: true
 
     }
   },
 
   methods:{
+
+    // Funzione tasti prev e next
     nextPrev(isNext){
       isNext ? this.counter++ : this.counter--;
       if(this.counter === this.images.length) this.counter = 0;
       if(this.counter < 0) this.counter = this.images.length -1;
       console.log(this.counter)
+    },
+
+    // Funzione Autoscroll
+    startAutoscroll(){
+      setInterval(() => {
+        if(this.autoScroll) this.nextPrev(true)
+      },3000)
     }
 
   },
+
+  mounted(){
+    
+    this.startAutoscroll()
+
+  }
 
 
 
